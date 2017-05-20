b2c382f81f8b3ce95e1c87bb1decf46100bdc64a
ruudbes <ruudbes>
2017-05-20 14:25:24 +0200

    initial commit

---
+++ Assets/Scripts/CubeCommands.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class CubeCommands : MonoBehaviour
+{
+    Vector3 originalPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Grab the original local position of the cube when the app starts.
+        originalPosition = this.transform.localPosition;
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        // If the cube has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+
+    // Called by SpeechManager when the user says the "Reset world" command
+    void OnReset()
+    {
+        // If the cube has a Rigidbody component, remove it to disable physics.
+        var rigidbody = this.GetComponent<Rigidbody>();
+        if (rigidbody != null)
+        {
+            DestroyImmediate(rigidbody);
+        }
+
+        // Put the sphere back into its original local position.
+        this.transform.localPosition = originalPosition;
+    }
+
+    // Called by SpeechManager when the user says the "Drop cube" command
+    void OnDrop()
+    {
+        // Just do the same logic as a Select gesture.
+        OnSelect();
+    }
+}
---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour {
+
+    float distance = 10;
+
+    void OnMouseDrag()
+    {
+        Vector3 MousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(MousePosition);
+
+
+        transform.position = objPosition;
+
+    }
+}
+
+
